b4a0a95b1d1f8a62ee29d28cd5f0f8455047cd32
nikhilbishnoi <nikhilbishnoibhadugmail.com@nikhilbishnois-MacBook-Air.local>
2024-08-12 10:41:06 +0530

    added chnages in the code

--- src/components/Polygon.jsx
+++ src/components/Polygon.jsx
@@ -8,8 +8,8 @@ const Polygon = () => {
             <div className='flex justify-center pt-[160px] max-lg:pt-[100px] max-xl:pt-[120px] max-md:pt-[80px] max-sm:pt-12'>
                 <h2 className='max-w-[652px] text-center font-medium text-5xl max-sm:leading-7 max-lg:text-[40px] max-lg:leading-[48px] max-md:text-3xl max-md:max-w-[500px] max-sm:text-2xl max-sm:max-w-[400px] leading-[57px] text-black font_gilroy_medium'>Tools and <span className='text-textBlue'>Platforms for Polygon</span> Annotations</h2>
             </div>
-            <img className='w-full pt-16 max-sm:pt-12 max-lg:pt-14' src={polygonFirst} alt="polygonFirst" />
-            <img className='w-full pt-6' src={polygonSecond} alt="polygonSecond" />
+            <img className='w-full pointer-events-none pt-16 max-sm:pt-12 max-lg:pt-14' src={polygonFirst} alt="polygonFirst" />
+            <img className='w-full pt-6 pointer-events-none' src={polygonSecond} alt="polygonSecond" />
         </div>
     </div>
   )
