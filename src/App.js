62c7375ab201af1e47caeaa8a4181500ee0e7bcc
AmanPunia-510 <puniaaman888@gmail.com>
2024-08-13 09:19:51 +0530

    work on router in the page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import './App.css';
-import Exclusive from './components/Exclusive';
-import Faqs from './components/Faqs';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Label from './components/Label';
-import Polygon from './components/Polygon';
-import Quality from './components/Quality';
-import Segmentation from './components/Segmentation';
-import System from './components/System';
-import Team from './components/Team';
-import BottomTop from './components/BottomTop'
+import "./App.css";
+import Exclusive from "./components/Exclusive";
+import Faqs from "./components/Faqs";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Label from "./components/Label";
+import Polygon from "./components/Polygon";
+import Quality from "./components/Quality";
+import Segmentation from "./components/Segmentation";
+import System from "./components/System";
+import Team from "./components/Team";
+import BottomTop from "./components/BottomTop";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-  <div className=' overflow-hidden'>
-  <Header/>
-  <Label/>
-  <Segmentation/>
-  <Team/>
-  <System/>
-  <Quality/>
-  <Polygon/>
-  <Faqs/>
-  <Exclusive/>
-  <Footer/>
-<BottomTop/>
-  </div>
+    <div className=" overflow-hidden">
+      <Header />
+      <Routes>
+        <Route path="/label" element={<Label />} />
+        <Route path="/about-us" element={<Segmentation />} />
+        <Route path="/how-it-work" element={<Quality />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/faq" element={<Faqs />} />
+      </Routes>
+      <Label />
+      <Segmentation />
+      <Team />
+      <System />
+      <Quality />
+      <Polygon />
+      <Faqs />
+      <Exclusive />
+      <Footer />
+      <BottomTop />
+    </div>
   );
 }
 
