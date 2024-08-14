3156fc795564bbba3d11774bad9dbb5a0bbe6921
AmanPunia-510 <puniaaman888@gmail.com>
2024-08-14 14:49:55 +0530

    update done

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Steps } from '../common/CommonHelper';
+import { STEPS } from '../common/Helper';
 
 const WorkflowSteps = () => {
   return (
     <div className="relative flex flex-col space-y-12 lg:p-6 sm:p-4 p-2 bg-white rounded-lg  mx-auto pl-5">
-      {Steps.map((val, index) => (
+      {STEPS.map((val, index) => (
         <div key={index} className="relative flex items-start">
           <div className="flex items-center justify-center max-w-12 min-h-12 w-full h-full rounded-full bg-[#E6EDF7]">
             {val.icon}
@@ -13,7 +13,7 @@ const WorkflowSteps = () => {
             <h3 className="text-lg font-semibold text-black">{val.title}</h3>
             <p className="pt-3 max-w-[400px] text-sm text-grey">{val.description}</p>
           </div>
-          {index < Steps.length - 1 && (
+          {index < STEPS.length - 1 && (
             <div className="absolute left-6 top-16 h-full border-l-2 border-dotted border-blue-200 transform -translate-y-6"></div>
           )}
         </div>
@@ -21,4 +21,4 @@ const WorkflowSteps = () => {
     </div>
   );
 };
-export default Timeline;
+export default WorkflowSteps;
